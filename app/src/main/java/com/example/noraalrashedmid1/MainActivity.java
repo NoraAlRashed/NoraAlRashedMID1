5dc82148fb2a72e5fd5b6046fd26803391f96308
NoraAlRashed <99714641+NoraAlRashed@users.noreply.github.com>
2022-02-21 12:51:26 +0300

    splash and icon and half of act1

--- app/src/main/java/com/example/noraalrashedmid1/MainActivity.java
+++ app/src/main/java/com/example/noraalrashedmid1/MainActivity.java
@@ -2,14 +2,69 @@ package com.example.noraalrashedmid1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        ImageView animal;
+        int playing;
+        MediaPlayer animalmp;
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //comment
+        setTitle("You're in the first activity");
+
+        animalmp = new MediaPlayer();
+        animalmp = MediaPlayer.create(this, R.raw.track1);
+
+        playing = 0;
+
+        animal = (ImageView) findViewById(R.id.imageView);
+
+        Button actbtn2 = findViewById(R.id.act2btn);
+        Button actbtn3 = findViewById(R.id.act3btn);
+
+
+        actbtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+            }
+        });
+        actbtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(MainActivity.this, MainActivity3.class));
+            }
+        });
+
+        animal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                switch (playing) {
+                    case 0:
+                        animalmp.start();
+                        playing = 1;
+                        break;
+                    case 1:
+                        animalmp.pause();
+                        playing = 0;
+                        break;
+                }
+            }
+        });
+
+    }
     }
 }
---
+++ app/src/main/java/com/example/noraalrashedmid1/MainActivity3.java
@@ -0,0 +1,69 @@
+package com.example.noraalrashedmid1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity3 extends AppCompatActivity {
+    {
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main3);
+        setTitle("You are in Activity3");
+
+        String[] array = {
+                "First Item",
+                "Second Item",
+                "Third Item",
+                "Fourth Item",
+                "GO TO ACT 1",
+                "GO TO ACT 2"};
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_main3, R.id.travel, array));
+
+    }
+
+        protected void onListItemClick(
+            ListView l, View v, int position,long id) {
+        super.onListItemClick(l, v, position, id);
+
+        switch (position) {
+            case 0:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://alfaisal.edu")));
+                break;
+
+            case 1:
+                startActivity(new Intent(MainActivity3.this, BigItem.class));
+                break;
+
+            case 2:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://amazon.com")));
+                break;
+
+            case 3:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://apple.com")));
+                break;
+
+            case 4:
+                startActivity(new Intent(MainActivity3.this, MainActivity.class));
+                break;
+
+            case 5:
+                startActivity(new Intent(MainActivity3.this, MainActivity2.class));
+                break;
+        }
+
+    }
+    }
+
+}
